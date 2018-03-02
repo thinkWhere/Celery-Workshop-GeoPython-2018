d33ba1f364b6afac9ce301cb5349353bd69a77ff
mattkernow <walsh.gis@gmail.com>
2018-03-02 11:54:39 +0000

    Use a task base class

--- celery-app/app/tasks.py
+++ celery-app/app/tasks.py
@@ -1,14 +1,30 @@
 from celery_app import app
 from time import sleep
+from celery import Task
 
 
-@app.task
-def add(x, y):
+class AppBaseTask(Task):
     """
-    Simple task which adds two numbers
+
+    """
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        print('{0!r} failed: {1!r}'.format(task_id, exc))
+
+
+@app.task(base=AppBaseTask, bind=True)
+def add(self, x, y):
+    """Simple task which adds two numbers
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (str): The second parameter.
+
+    Returns:
+        bool: The return value. True for success, False otherwise.
+
     :param x: integer
     :param y: integer
-    :return: result of x + y
+    @:returns result of x + y
     """
     sleep(3)
     result = x + y
