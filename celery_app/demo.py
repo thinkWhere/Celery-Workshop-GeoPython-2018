bb14a726b8f195bbffab2a07bc204764d3dd0e65
Ian Feeney <ian.feeney@thinkwhere.com>
2018-03-28 22:57:57 +0100

    wip

--- celery_app/demo.py
+++ celery_app/demo.py
@@ -1,20 +1,23 @@
 from app.tasks import add_task
-
+import random
 
 iterations = 30
-
 
 def call_add_number_task():
     """Call the add task x times."""
     global iterations
+    x_max = 700000
+    y_max = 1300000
 
     for task_execution in range(iterations):
 
-        other_num = 3 + iterations
-        add_task.delay(iterations, other_num)
+        x = random.randint(0, x_max)
+        y = random.randint(0, y_max)
 
-        print(f"called add({iterations}, {other_num})")
-        iterations = other_num
+        add_task.delay(x, y)
+
+        print(f"called add({x}, {y})")
+
 
 
 if __name__ == '__main__':
