43fb0ce598a1b72ea76aab64efd934d3549d0f7f
mattkernow <walsh.gis@gmail.com>
2018-04-13 15:33:48 +0100

    init connection when needed

--- celery_app/app/db.py
+++ celery_app/app/db.py
@@ -4,17 +4,26 @@ from psycopg2.pool import ThreadedConnectionPool
 from psycopg2.extras import RealDictCursor
 
 
-pool = ThreadedConnectionPool(1, 4,
-                              dbname="geopython-db",
-                              user="geopython",
-                              host="postgis",
-                              port="5432",
-                              password="geopython")
+pool = None
 
+def create_connection_pool():
+    """Create a connection pool for the worker"""
+    pool = ThreadedConnectionPool(1, 4,
+                                  dbname="geopython-db",
+                                  user="geopython",
+                                  host="postgis",
+                                  port="5432",
+                                  password="geopython")
+    return pool
 
 @contextmanager
 def get_db_connection():
     try:
+        # Setup connection pool is it doesn't exist
+        global pool
+        if pool is None:
+            pool = create_connection_pool()
+
         connection = pool.getconn()
         yield connection
     finally:
