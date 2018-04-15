beff95565be849a5b5be59698e5c6ee88c973960
mattkernow <walsh.gis@gmail.com>
2018-04-15 14:09:11 +0100

    fix tests

--- celery_app/tests/tests_unit/test_tasks.py
+++ celery_app/tests/tests_unit/test_tasks.py
@@ -1,12 +1,13 @@
-from pytest import raises
 from unittest.mock import patch
 
 from celery.exceptions import Retry
+from pytest import raises
 
-from app.tasks import do_task
+from app.service import ServiceError
+from app.tasks import do_task, TaskError
 
 
-@patch('app.tasks.add')
+@patch('app.tasks.geoprocess')
 def test_do_task_success(mock_add):
     # Act
     do_task(2, 3)
@@ -15,14 +16,27 @@ def test_do_task_success(mock_add):
     mock_add.assert_called_with(2, 3)
 
 
-@patch('app.tasks.add')
-@patch('app.tasks.add_task.retry')
-def test_do_task_handles_failure(mock_add_task_retry, mock_add):
+@patch('app.tasks.geoprocess')
+@patch('app.tasks.do_task.retry')
+def test_do_task_handles_serviceerror_with_retry(mock_add_task_retry, mock_geoprocess):
 
     # Arrange
     mock_add_task_retry.side_effect = Retry()
-    mock_add.side_effect = NotImplementedError('Random fail!')
+    mock_geoprocess.side_effect = ServiceError('Random fail!')
 
     # Act/assert
     with raises(Retry):
+        do_task(2, 1)
+
+
+@patch('app.tasks.geoprocess')
+@patch('app.tasks.do_task.retry')
+def test_do_task_handles_unexpected_exception(mock_add_task_retry, mock_geoprocess):
+
+    # Arrange
+    mock_add_task_retry.side_effect = Retry()
+    mock_geoprocess.side_effect = NotADirectoryError('Random fail!')
+
+    # Act/assert
+    with raises(TaskError):
         do_task(2, 1)
